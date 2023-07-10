665e9b608f3107e31e17d64f7ea951da738587b3
MotionSoft05 <kevinbbarrios05@gmail.com>
2023-07-09 23:59:55 -0300

    Unedit Routes

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
       <Route exact path="/" render={() => <Landing />} />
       <Route path="/home" render={() => <Home />} />
       <Route path="/detail/:id" render={() => <Detail />} />
+      <Route path="/create" render={() => <Form />} />
     </div>
   );
 }
